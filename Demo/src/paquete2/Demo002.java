d2b8c1d0761a6041010d7ddf06630ae158518a17
Byron Alvarez <bvalvarez1.1@gmail.com>
2024-04-11 17:17:11 -0500

    Taller 00

--- Demo/src/paquete2/Demo002.java
+++ Demo/src/paquete2/Demo002.java
@@ -7,21 +7,23 @@ import java.util.Scanner;
 public class Demo002 {
 
     public static void main(String[] args) {
-        // Usamos el printf para presentar la oracion, en el %s ponemos el string
-        // que obtenemos de la clase "Mensaje", seguido de eso usamos %d para 
-        // presentar por pantalla el numero que obtenemos de la operacion
-        // del paquete "Informacion"
         Scanner sc = new Scanner(System.in);
         System.out.print("¿Desea que la cadena de texto se imprima en minusculas?"
                 + " Si es así digite cualquier numero entre 1 a 5: ");
         int num = sc.nextInt();
+        // Usamos el printf para presentar la oracion, en el %s ponemos el string
+        // que obtenemos de la clase "Mensaje", seguido de eso usamos %d para 
+        // presentar por pantalla el numero que obtenemos de la operacion
+        // del paquete "Informacion"
+
         if (num >= 1 && num <= 5) {
             System.out.printf("%s con %d\n", Mensaje.obtenerMensaje(),
                     Informacion.obtenerHabitantes());
+            // Si el numero está entre 1 y 5, el texto saldrá en minusculas
         } else {
             System.out.printf("%s CON %d\n", Mensaje.obtenerMensaje().toUpperCase(),
                     Informacion.obtenerHabitantes());
-        }
+        } // Sino la cadena se imprimirá en mayusculas
 
         // usar printf;
     }
